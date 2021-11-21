88d3ebd0c3df269e97556bfeaa4d19cc224bf980
นายพุฒิพงศ์  สักแสน <59078952+Senju14589@users.noreply.github.com>
2021-11-22 04:29:46 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
 export default function Header(props) {
-    return (
-        <header className="block row center">
-          <div>
-            <a href="#/">
-              <h1>Small Shopping Cart</h1>
-            </a>
-          </div>
-          <div>
-            <a href="#/cart">
-              Cart{' '}
-              {props.countCartItems ? (
-                <button className="badge">{props.countCartItems}</button>
-              ) : (
-                ''
-              )}
-            </a>{' '}
-            
-          </div>
-        </header>
-      );
+  return (
+    <header className="block row center">
+      <div>
+        <a href="#/">
+          <h1>NFT Shopping Cart</h1>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart">
+          Cart{' '}
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ''
+          )}
+        </a>{' '}
+
+      </div>
+    </header>
+  );
 }
