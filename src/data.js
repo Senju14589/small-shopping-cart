07c78c8cbef9c6992878686fcd5b2d70a1dd8acc
นายพุฒิพงศ์  สักแสน <59078952+Senju14589@users.noreply.github.com>
2021-11-22 03:24:52 +0700

    sae

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Stawberry',
+      price: 350,
+      image:'https://picsum.photos/id/1080/6858/4574',
+    },
+    {
+      id: '2',
+      name: 'Cactus',
+      price: 30,
+      image: 'https://picsum.photos/id/248/3872/2592',
+    },
+    {
+      id: '3',
+      name: 'W Shoes',
+      price: 500,
+      image: 'https://picsum.photos/id/102/4320/3240',
+    },
+  ],
+};
+export default data;
