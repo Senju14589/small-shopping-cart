ab0594dadfe1c5f13f88034bda37ae5cf35b1dc1
นายพุฒิพงศ์  สักแสน <59078952+Senju14589@users.noreply.github.com>
2021-11-22 03:44:05 +0700

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,21 +2,21 @@ const data = {
   products: [
     {
       id: '1',
-      name: 'Stawberry',
-      price: 350,
-      image:'https://picsum.photos/id/1080/6858/4574',
+      name: 'NFT : Mr.Macong',
+      price: 0.65,
+      image: 'https://cdn.vox-cdn.com/thumbor/9LceT_Csp47cr7Huai1lAP_3txs=/0x0:1380x1728/1120x0/filters:focal(0x0:1380x1728):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/22341226/Screen_Shot_2021_03_02_at_3.43.12_PM.png',
     },
     {
       id: '2',
-      name: 'Cactus',
-      price: 30,
-      image: 'https://picsum.photos/id/248/3872/2592',
+      name: 'NFT : MR.ringky',
+      price: 0.05,
+      image: 'https://scontent.fbkk5-7.fna.fbcdn.net/v/t39.30808-6/256007548_6742389612468500_8493771237207625887_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=b9115d&_nc_eui2=AeGBJSc5p1u5wMLmLODoq_RCJtmRoDCxzsEm2ZGgMLHOwUYjO9loRB1PunJKlYWrSZI5bIZBh-mcGbRPn_aJAE2Y&_nc_ohc=rCmj6x7gigwAX9ZEKQU&_nc_ht=scontent.fbkk5-7.fna&oh=8b34cc7a3632ba10453abaaac9cf82f7&oe=619F0B86',
     },
     {
       id: '3',
-      name: 'W Shoes',
-      price: 500,
-      image: 'https://picsum.photos/id/102/4320/3240',
+      name: 'NFT : Mr.Maboro',
+      price: 0.56,
+      image: 'https://picsum.photos/id/1https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/258601495_1142652583170427_7990272717370642685_n.jpg?_nc_cat=111&_nc_rgb565=1&ccb=1-5&_nc_sid=b9115d&_nc_eui2=AeFl8AJCOHN2FRDdBiCOIDklujWwC3_NOWy6NbALf805bB1dLvvrzT-3Hth6jd4RHV58Y65Et0HLZaSPOHjiMDYV&_nc_ohc=_q2kzGUG0C4AX_yl2J8&_nc_ht=scontent.fbkk5-3.fna&oh=b37d64cfd35b68f3d423ad125cbdf7f9&oe=61A00EA402/4320/3240',
     },
   ],
 };
